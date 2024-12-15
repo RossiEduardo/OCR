345028980e5b964476654b676cd878311b4e3793
RossiEduardo <eduardorossi80@hotmail.com>
2024-12-14 23:18:06 -0300

    fix: get text

--- backend/src/openai/openai.controller.ts
+++ backend/src/openai/openai.controller.ts
@@ -1,15 +1,18 @@
 import { Controller, Post, Get } from '@nestjs/common';
 import { OpenaiService } from './openai.service';
+import { DocumentsService } from '@services/documents.service';
 
 @Controller('openai')
 export class OpenaiController {
     constructor(
         private readonly openaiService: OpenaiService,
+        private readonly documentsService: DocumentsService
     ) {}
     
     @Get('')
-    async generateExplanation(): Promise<{content: string}> {
-        let content = await this.openaiService.generateExplanation();
+    async generateExplanation(filename: string): Promise<{content: string}> {
+        let text = await this.documentsService.getExtractedText(filename);
+        let content = await this.openaiService.generateExplanation(text);
 
         return {content: content};
     }
--- backend/src/openai/openai.service.ts
+++ backend/src/openai/openai.service.ts
@@ -7,13 +7,9 @@ import OpenAI from "openai";
 export class OpenaiService {
     private readonly openai = new OpenAI({apiKey: process.env.OPEN_AI_SECRET_KEY});
         
-    private readonly openaiApiKey = process.env.OPENAI_API_KEY;  // Chave de API OpenAI
-    private readonly openaiEndpoint = 'https://api.openai.com/v1/completions';  // Endpoint da API OpenAI
-
     // Função para gerar explicações sobre o texto
-    async generateExplanation(): Promise<string> {
+    async generateExplanation(text: string): Promise<string> {
         try {
-            let text = "O  Programa,  a  CPG-ICMC  e  o  ICMC-USP  não  se  responsabilizam  pelo  não  preenchimento  da  inscrição  por \nmotivo  de  ordem  técnica  referente  aos  computadores,  falhas  de  comunicação,  congestionamento  das  linhas  de \ncomunicação,  bem  como  outros  fatores  que  impossibilitem  a  transferência  de  dados  e  documentos.  Não  se \nresponsabiliza, ainda, por qualquer tipo de problema ou crime cibernético, que resulte na não efetivação da inscrição. \n7.4. Os  casos  omissos,  isto  é,  que  não  estão  contemplados  neste  documento,  serão  resolvidos  pela  Comissão \nCoordenadora do Programa.";
             const stream = await this.openai.chat.completions.create({
                 model: "gpt-3.5-turbo",
                 messages: [{ role: "user", content: `Por favor, explique o seguinte texto:\n\n${text}` }],
@@ -24,23 +20,6 @@ export class OpenaiService {
                 content += (chunk.choices[0]?.delta?.content || "");
             }
             return content;
-            // const response = await axios.post(
-            //     this.openaiEndpoint,
-            //     {
-            //     model: 'gpt-4',  // Modelo GPT-4, mas pode ser substituído por outro modelo
-            //     prompt: `Por favor, explique o seguinte texto:\n\n${text}`,
-            //     max_tokens: 150,  // Tamanho da resposta do modelo
-            //     temperature: 0.7,  // Controle de criatividade (0 a 1)
-            //     },
-            //     {
-            //     headers: {
-            //         'Content-Type': 'application/json',
-            //         Authorization: `Bearer ${this.openaiApiKey}`,
-            //     },
-            //     }
-            // );
-            // console.log(response.data.choices[0].text.trim()); // Retorna a explicação gerada
-
         } catch (error) {
             console.error('Erro ao gerar explicação:', error);
             throw new Error('Falha ao interagir com o modelo de linguagem');
